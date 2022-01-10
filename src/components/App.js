3f989a901ec334452ba86916bff170f019e9a8c8
sebastiangraz <graz@live.se>
2022-01-10 23:19:32 +0100

    v1 notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,69 @@
 /** @jsxImportSource theme-ui */
 
-import * as componentList from "./SlideIndex";
+import * as componentList from "../slides";
 import Slides from "./Slides";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-
+import Notes from "./Notes";
 import { chromeColors } from "../theme";
+
+const slides = [
+  { notes: "Intro slide notes", component: componentList.SlideIntro },
+  { notes: "Skills slide notes", component: componentList.SlideSkills },
+  { notes: "Clients slide notes", component: componentList.SlideClients },
+  {
+    notes: "CaseCapchase slide notes",
+    component: componentList.SlideCaseCapchase,
+  },
+  { notes: "CaseLoupe slide notes", component: componentList.SlideCaseLoupe },
+  {
+    notes: "CasePrototypingGuide slide notes",
+    component: componentList.SlideCasePrototypingGuide,
+  },
+  { notes: "Process slide notes", component: componentList.SlideProcess },
+  { notes: "Ethos slide notes", component: componentList.SlideEthos },
+  {
+    notes: "Exploration slide notes",
+    component: componentList.SlideExploration,
+  },
+];
 
 const isSafari =
   navigator.userAgent.indexOf("Safari") !== -1 &&
   navigator.userAgent.indexOf("Chrome") === -1;
+
+const showSlides = () => {
+  if (window.location.pathname === "/") {
+    return (
+      <>
+        <Slides>
+          {Object.entries(slides).map(([i, v]) => {
+            const Slide = v.component;
+            return <Slide key={i} notes={v.notes} />;
+          })}
+        </Slides>
+        {Object.entries(slides).map(([i]) => {
+          return (
+            <div
+              style={{
+                height: "100vh",
+                width: "100vw",
+                scrollSnapAlign: "start",
+              }}
+              className="ghostSlide"
+              key={i}
+            />
+          );
+        })}
+      </>
+    );
+  }
+};
+
+const showNotes = (e) => {
+  if (window.location.pathname === "/notes") {
+    return <Notes />;
+  }
+};
 
 const App = () => {
   return (
@@ -30,24 +85,8 @@ const App = () => {
             media="(prefers-color-scheme: dark)"
           />
         </Helmet>
-        <Slides>
-          {Object.entries(componentList).map(([i, Slide]) => {
-            return <Slide key={i} />;
-          })}
-        </Slides>
-        {Object.entries(componentList).map(([i]) => {
-          return (
-            <div
-              style={{
-                height: "100vh",
-                width: "100vw",
-                scrollSnapAlign: "start",
-              }}
-              className="ghostSlide"
-              key={i}
-            />
-          );
-        })}
+        {showSlides()}
+        {showNotes()}
       </div>
     </HelmetProvider>
   );
---
+++ src/components/Notes.js
@@ -0,0 +1,13 @@
+/** @jsxImportSource theme-ui */
+
+import React from "react";
+
+const Notes = ({ data }) => {
+  return (
+    <>
+      <h1>Notes, {data}</h1>
+    </>
+  );
+};
+
+export default Notes;
