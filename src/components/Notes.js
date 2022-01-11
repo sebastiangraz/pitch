2a903ccd8373c62a03890d190d7145a18551ea21
sebastiangraz <graz@live.se>
2022-01-11 21:49:21 +0100

    update code for deploy

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,7 @@ import React from "react";
 import { Padding } from "./Padding";
 import io from "socket.io-client";
 
-const socket = io("ws://localhost:8080");
+const socket = io("https://brandpresentation.herokuapp.com/");
 
 const Notes = () => {
   const [note, getNote] = React.useState("test");
