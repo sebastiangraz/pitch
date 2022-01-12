c2f76a72939914b97f07c8480c49805f2b6bd549
Sebastian Graz <graz@graz.fritz.box>
2022-01-13 00:03:03 +0100

    clean slate before deploy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
-const http = require("http").createServer();
 const express = require("express");
-const io = require("socket.io")(http, {
-  cors: { origin: "*" },
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 8080;
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => {
+  console.log(`server is running ${PORT}`);
 });
+
+app.use(router);
 
 io.on("connection", (socket) => {
   socket.on("message", (message) => {
+    console.log(message);
     io.emit("emit", `${message}`);
   });
 });
-
-http.listen(process.env.PORT || 8080, () => console.log("listen 8080"));
