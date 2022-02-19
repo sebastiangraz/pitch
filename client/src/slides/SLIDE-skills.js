5616aa0d8621f15c98c933ac2161e53b55f8bbd8
sebastiangraz <graz@live.se>
2022-02-19 18:15:39 +0100

    Add images to skills

--- client/src/slides/SLIDE-skills.js
+++ client/src/slides/SLIDE-skills.js
@@ -5,6 +5,7 @@ import { Padding } from "../components/Padding";
 import Reveal from "../components/Reveal";
 import { globalStyle } from "../components/globalStyles";
 import { mix } from "@theme-ui/color";
+import React from "react";
 
 const skills = [
   "Branding",
@@ -23,12 +24,19 @@ const skills = [
 ];
 
 export const SlideSkills = () => {
+  const [image, setImage] = React.useState();
   return (
     <Padding constrain>
       <Reveal
         {...globalStyle.textRevealAnimation}
         sx={{
-          "&>*": {
+          position: "relative",
+          zIndex: 1000,
+          "&:hover *:not(:hover)": {
+            color: "rgba(0,0,0,0)",
+            cursor: "pointer",
+          },
+          "& > *": {
             display: "inline-block",
             mr: 2,
             mb: 1,
@@ -37,24 +45,56 @@ export const SlideSkills = () => {
       >
         {skills.map((e, i) => {
           return (
-            <Text
-              key={e}
-              sx={{
-                color: (t) => `
+            <>
+              <Text
+                onMouseEnter={() => {
+                  const fileName = e.toLowerCase().split(" ").join("-");
+                  const requireFile = require(`../assets/skills/${fileName}.png`);
+                  setImage(requireFile);
+                }}
+                onMouseLeave={() => {
+                  setImage("");
+                }}
+                key={e}
+                sx={{
+                  transition: "ease color 0.2s",
+                  "&:hover": {
+                    color: "#fff",
+                    cursor: "pointer",
+                    position: "relative",
+                    zIndex: 1000,
+                  },
+                  color: (t) => `
                   ${mix(
                     "bg",
                     "text",
                     0.01 + i * (100 / skills.length) * 0.01
                   )(t)}
               `,
-              }}
-              variant="heading"
-            >
-              {e} ·{" "}
-            </Text>
+                }}
+                variant="heading"
+              >
+                {e} ·{" "}
+              </Text>
+            </>
           );
         })}
       </Reveal>
+      {image && (
+        <img
+          sx={{
+            pointerEvents: "none",
+            position: "absolute",
+            top: "2em",
+            left: "2em",
+            width: "calc(100% - 4em)",
+            height: "calc(100% - 4em)",
+            objectFit: "contain",
+          }}
+          src={image}
+          alt={image}
+        />
+      )}
     </Padding>
   );
 };
